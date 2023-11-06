8fb3cd79b6975835ce510338aaac23d431bed2f9
Ali Hussain <alihhussain@live.com>
2023-11-06 17:52:16 -0600

    changes

---
+++ CloudJourneyLabs/AppGWLab/setupscript.sh
@@ -0,0 +1,7 @@
+apt update
+apt install python3-pip
+pip3 install Flask
+apt install net-tools
+
+# Get the website file
+wget https://raw.githubusercontent.com/alihhussain/CloudJourney/main/CloudJourneyLabs/AppGWLab/webserver.py    
