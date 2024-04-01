38d43aeab9038ee0520c60239452031b34005297
Ali Hussain <youremail@example.com>
2024-04-01 20:17:37 +0000

    list

--- terraform/pipelinevnet/variables.tf
+++ terraform/pipelinevnet/variables.tf
@@ -15,7 +15,7 @@ variable "vnet_name" {
 
 variable "address_space" {
   description = "The address space of the virtual network"
-  type        = string
+  type        = list(string)
   default     = "10.0.0.0/16"
 }
 
