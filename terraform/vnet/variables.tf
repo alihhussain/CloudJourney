5e29ea4703b496f299b0539b4f19c47713aa80ef
Ali Hussain <youremail@example.com>
2024-04-01 18:26:24 +0000

    adding terraform

---
+++ terraform/vnet/variables.tf
@@ -0,0 +1,30 @@
+variable "location" {
+  description = "The location where resources will be created"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space of the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_names" {
+  description = "A list of subnet names"
+  type        = list(string)
+}
+
+variable "subnet_prefixes" {
+  description = "A list of subnet prefixes that must be within the Virtual Network address space"
+  type        = list(string)
+}
