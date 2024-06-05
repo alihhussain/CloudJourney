ca923605c78eaae33cd9aa67a58ace758828259d
bilal01 <bilal01@example.com>
2024-06-05 03:46:03 +0000

    commit terraform

---
+++ BilalsFolder/Terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+location             = "East US"
+resource_group_name  = "BilalResourceGroup"
+vnet_name            = "Bilalpr2VNet"
+address_space        = ["10.0.0.0/21"]
+subnet_name         = "Bilalsub1"
+subnet_prefix      = ["10.0.1.0/24"]
---
+++ BilalsFolder/Terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "location" {
+  description = "The location where resources will be created"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space of the virtual network"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "A list of subnet names"
+  type        = string
+}
+
+variable "subnet_prefix" {
+  description = "A list of subnet prefixes that must be within the Virtual Network address space"
+  type        = list(string)
+}
