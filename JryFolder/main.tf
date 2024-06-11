f52fed48be8048e6894e30462936e615fede1d9d
Jry <jry@example.com>
2024-06-11 04:54:26 +0000

    jry terraform template

---
+++ JryFolder/main.tf
@@ -0,0 +1,109 @@
+
+provider "azurerm" {
+  features {}
+  version = "~> 2.0"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "jryResourceGroup"
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "jryLinuxVM"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "jryuser"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_DS1_v2"
+}
+
+variable "admin_ssh_key" {
+  type    = string
+  default = "ssh-rsa"
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "jryVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "jrySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                = "jryPublicIP"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "jryNIC"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "jryNICConfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.admin_ssh_key
+  }
+
+  disable_password_authentication = true
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.publicip.ip_address
+}
