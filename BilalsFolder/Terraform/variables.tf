97b598012d8bdf75812da61ae4a61191f785334b
bilal01 <bilal01@example.com>
2024-06-12 02:23:25 +0000

    rebase

--- BilalsFolder/Terraform/variables.tf
+++ BilalsFolder/Terraform/variables.tf
@@ -2,7 +2,7 @@ variable "location" {
   description = "The location where resources will be created"
   type        = string
 }
-
+az
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
